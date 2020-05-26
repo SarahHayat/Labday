a4bf98fee8e312a8a6cb6ff7b1416289d2a64e71
tchydemian <marietchydemian@edu.itescia.fr>
2020-05-26 15:20:18 +0200

    add informations accueil

--- php/index.php
+++ php/index.php
@@ -76,12 +76,17 @@ if(isset($_SESSION['username'])){
 ?>
     <section class="fond">
         <div>
-            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des
-                morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les
-                années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
-            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des
-                morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les
-                années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+            <p>QUI SOMMES-NOUS ?  </p>
+            <p> Un groupe d’étudiant qui souhaite divertir les gens. Enfaite on aime faire des activités sympas et découvrir de nouvelles choses. On a donc souhaité faire rencontrer les gens avec des activités, aider les gens à sortir autrement. </p>
+        </div>
+        <div>
+            <p>POUR QUI ?  </p>
+            <p> Ce site est accessible pour les professionnels de l’évènement comme pour les particuliers qui souhaitent promouvoir des évènements ou simplement les utilisateurs qui souhaite participer.  </p>
+        </div>
+        <div>
+            <p>VOUS SOUHAITEZ ?  </p>
+            <p> Faire quelque chose de nouveau ou apprendre?
+                Vous ne savez pas quoi faire avec votre enfant, vote soeur ou bien vos copines pour sortir ? Trouver des évènements, de tous genre, autour de vous, avec des utilisateurs notés grâce à leurs évènements précédents.  </p>
         </div>
     </section>
 
