fffac91d951210ce400b3f84d66d1d35d0e23eca
Debailleux Jonathan <jonathandebailleux@yahoo.fr>
2020-05-26 10:33:31 +0200

    stable version forum

--- php/page_forum.php
+++ php/page_forum.php
@@ -23,26 +23,28 @@ if (isset($_SESSION['username'])) {
 
 }
 
-
-
-
-$reponse = $bdd->query('SELECT * FROM sujets_forum as sf join messages as m on sf.id_sujet = m.id_sujet
-left join utilisateurs as u on sf.id_utilisateur = u.id_utilisateur WHERE sf.id_sujet ="'.$_GET['id_sujet'].'" ');
+$req = $bdd->query('SELECT nom_sujet FROM sujets_forum  WHERE id_sujet ="'.$_GET['id_sujet'].'" ');
+$donnees = $req->fetch();
+?>
+<div>
+    <h3><?php echo $donnees['nom_sujet'] ?></h3>
+</div>
+<?php
+$reponse = $bdd->query('SELECT m.id_utilisateur, m.message, sf.nom_sujet, u.pseudo,m.date_message FROM sujets_forum as sf join messages as m on sf.id_sujet = m.id_sujet
+left join utilisateurs as u on m.id_utilisateur = u.id_utilisateur WHERE m.id_sujet ="'.$_GET['id_sujet'].'" ');
 while ($donnees = $reponse->fetch()) {
 ?>
 
 <div class="evenement">
-    <form method="post" action="reponseForum.php">
-        <div>
-            <input name="sujet" value="<?php echo $donnees['nom_sujet'] ?>">
-        </div>
+    <form method="post" action="reponseForum.php?id_sujet=<?php echo $_GET['id_sujet'] ?>">
 
         <div class="evenement">
 
             <div class="listOfEvent">
                 <ul class="collectionItem">
                     <div class="pictureEvent">
-                        <p><?php echo "Par " . '<b>' . $donnees['id_utilisateur'] . '</b>' ?></p>
+                        <p><?php echo "Par " . '<b>' . $donnees['pseudo'] . '</b>' ?></p>
+                        <p> <?php echo "Le " . $donnees['date_message'] ?> </p>
                         <p><?php echo $donnees['message']; ?></p>
 
                     </div>
