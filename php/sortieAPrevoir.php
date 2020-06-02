d48ed852e156debf34b9187d217a5e6c518de191
tchydemian <marietchydemian@edu.itescia.fr>
2020-06-02 17:10:20 +0200

    Auto stash before merge of "master" and "origin/master"

--- php/sortieAPrevoir.php
+++ php/sortieAPrevoir.php
@@ -8,8 +8,14 @@ session_start();
     <meta charset="UTF-8">
     <title>ShareEventTogether - Sorties à prévoir</title>
     <link rel="stylesheet" href="../assets/css/sortie.css"/>
+<<<<<<< Updated upstream
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"/>
 
+=======
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+          crossorigin=""/>
+>>>>>>> Stashed changes
 </head>
 
 
@@ -33,9 +39,16 @@ require("../controllers/bdd.php");
 
 ?>
 <div id="mapid"></div>
+<<<<<<< Updated upstream
 <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
 <script src="../js/mapping.js"></script>
 
+=======
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+        integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+        crossorigin=""></script>
+<script type="text/javascript" src="../js/mapping.js"></script>
+>>>>>>> Stashed changes
 
 <form action="filtreAPrevoir.php" method="post" class="filtre">
 
