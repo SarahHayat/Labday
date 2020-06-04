9c24f345c27e48169ba04e2212a1895d25955c65
Debailleux Jonathan <jonathandebailleux@yahoo.fr>
2020-06-04 16:24:19 +0200

    debut api gouv

--- controllers/createEvent.php
+++ controllers/createEvent.php
@@ -13,18 +13,22 @@ require ("bdd.php");
         $description = $_POST["description"];
         $date_evenement = $_POST["date_evenement"];
         $titre = $_POST["titre"];
-        $lieu = $_POST["lieux"];
+        $adresse = $_POST["adresse"];
+        $code_postale = $_POST["code_postale"];
+        $commune = $_POST["commune"];
         $categorie = $_POST['categorie'];
 
 
-        if (isset($_SESSION['id_name']) && isset($description) && isset($date_evenement) && isset($titre) && isset($lieu) && isset($_SESSION['username']) && isset($categorie)) {
-            $req = $bdd->prepare('INSERT INTO evenements(id_utilisateur, description, date_evenement, titre_evenement, lieu, id_categorie) VALUES(:utilisateur, :description, :date_evenement, :titre, :lieu, :categorie)');
+        if (isset($_SESSION['id_name']) && isset($description) && isset($date_evenement) && isset($titre) && isset($adresse) && isset($code_postale) && isset($commune) && isset($_SESSION['username']) && isset($categorie)) {
+            $req = $bdd->prepare('INSERT INTO evenements(id_utilisateur, description, date_evenement, titre_evenement, adresse, code_postale, commune, id_categorie) VALUES(:utilisateur, :description, :date_evenement, :titre, :adresse, :code_postale, :commune, :categorie)');
             $req->execute(array(
                 'utilisateur' => $_SESSION['id_name'],
                 'description' => $description,
                 'date_evenement' => $date_evenement,
                 'titre' => $titre,
-                'lieu' => $lieu,
+                'adresse' => $adresse,
+                'code_postale' => $code_postale,
+                'commune' => $commune,
                 'categorie' => $categorie,
 
             ));
--- js/mapping.js
+++ js/mapping.js
@@ -1,11 +1,25 @@
+// let long;
+// let lat;
+// let tab_long_lat = [long,lat];
+// var mymap = L.map('mapid').setView( tab_long_lat , 13);
+//
+// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: 'pk.eyJ1Ijoic2hhcmVldmVudCIsImEiOiJja2F4d2pvcmQwOGZ1MnFwNnQ0OTluMWQyIn0.YJ8RW6Z5-A-J3ttr38YiGQ'
+// }).addTo(mymap);
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoic2hhcmVldmVudCIsImEiOiJja2F4d2pvcmQwOGZ1MnFwNnQ0OTluMWQyIn0.YJ8RW6Z5-A-J3ttr38YiGQ'
-}).addTo(mymap);
+let adresse = document.getElementById("adresse").value;
+let codePostal = document.getElementById("code_postale").value; // pas sur pour la value possiblement .val ou autre
+let commune = document.getElementById('commune').value;
+
+let url = new URL("https://api-adresse.data.gouv.fr/search/");
+
+url.searchParams.append("q", adresse);
+url.searchParams.append("postcode", codePostal);
+url.searchParams.append("city" , commune);
+
