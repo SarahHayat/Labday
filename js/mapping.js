7de7e15f79b89ada46165ce00f49e69dc5b0541f
Jonathan <jonathandebailleux@yahoo.fr>
2020-06-05 11:49:03 +0200

    Auto stash before merge of "master" and "origin/master"

--- js/mapping.js
+++ js/mapping.js
@@ -1,6 +1,14 @@
 // var x = document.getElementById("coordinates-x").value;
 // var y = document.getElementById("coordinates-y").value;
 
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoic2hhcmVldmVudCIsImEiOiJja2F4d2pvcmQwOGZ1MnFwNnQ0OTluMWQyIn0.YJ8RW6Z5-A-J3ttr38YiGQ'
+}).addTo(mymap);
 
 var mapTab = [
     ["maison",49.1374,2.32753],
@@ -21,5 +29,4 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoic2hhcmVldmVudCIsImEiOiJja2F4d2pvcmQwOGZ1MnFwNnQ0OTluMWQyIn0.YJ8RW6Z5-A-J3ttr38YiGQ'
 }).addTo(mymap);
-
 
--- php/headerConnect.php
+++ php/headerConnect.php
@@ -6,8 +6,7 @@
     <title>ShareEventTogether</title>
     <link rel="stylesheet" href="../assets/css/headerConnect.css"/>
 </head>
-<div style="margin-bottom: 14%">
-</div>
+
 <header class="header">
     <div class="menuHaut" id="menuHaut">
         <h1 id="title"> ShareEventTogether</h1>
@@ -21,25 +20,28 @@
             if(isset($_SESSION['id_name']))echo $_SESSION['username'] ;?></h3>
 
     </div>
-    <div class="menuBas" id="nav-scroll">
-        <a href="index.php"> Accueil</a>
+<!--    <div class="menuBas" id="nav-scroll">-->
+<!--        <a href="index.php"> Accueil</a>-->
 <!--        <a href="sortieDuJour.php"> Sorties du jour</a>-->
-        <a href="minichat.php">Forum</a>
-        <a href="sortieAPrevoir.php"> Sorties à prévoir</a>
-        <a href="contact.php"> Contact</a>
-    </div>
+<!--        <a href="minichat.php">Forum</a>-->
+<!--        <a href="sortieAPrevoir.php"> Sorties à prévoir</a>-->
+<!--        <a href="contact.php"> Contact</a>-->
+<!--    </div>-->
+
 <script type="text/javascript">
    let lastSrollTop = 0;
+/*
    navbar = document.getElementById('nav-scroll');
+*/
    menuHaut = document.getElementById('menuHaut');
    window.addEventListener("scroll", function () {
        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
        if (scrollTop > lastSrollTop){
-            navbar.style.display="none";
-            menuHaut.style.borderBottom = "solid white 2px";
+           /* navbar.style.display="none";*/
+            menuHaut.style.background = "grey";
        }else {
-           navbar.style.display="flex";
-           menuHaut.style.borderBottom = "none";
+         /*  navbar.style.display="flex";*/
+           menuHaut.style.background = "none";
 
        }
        lastSrollTop = scrollTop;
